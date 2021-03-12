83e177c25b4744ce63ead7b06797e283b9296dff
Isaiah Guardado <37196217+crazay416@users.noreply.github.com>
2021-03-12 15:11:15 -0800

    Update main.c

--- Lab2/main.c
+++ Lab2/main.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+struct sort{
+	int upperBound;
+	int lowerBound;
+}
+
+int numArray[] = {7, 12, 19, 3, 18, 4, 2, 6, 15, 8};
+int arraySize = sizeof(numArray) / sizeof(numArray[0]);
+
+void* bubbleSort(void* argv){
+	int i;
+	struct sort halfOfArray = *(struct sort*) argv;
+	int high = halfOfArray.upperBound;
+	int low = halfOfArray.lowerBound;
+	for(i = low; high < )
+
+
+}
+
+
+int main(){
+	int i;
+	for(i = 0; i < arraySize; i++){
+		printf("%d\n", numArray[i]);
+	}
+
+}
