2621fd116af714878722bbee8cc854820577a123
Isaiah Guardado <37196217+crazay416@users.noreply.github.com>
2021-03-16 22:58:28 -0700

    Update main.c

--- Lab2/main.c
+++ Lab2/main.c
@@ -3,23 +3,38 @@
 #include <stdio.h>
 
 
+// Create a struct to divide the array from low to mid
+// and mid + 1 to sizeofArray
 struct sort{
 	int upperBound;
 	int lowerBound;
 };
 
+// Create the array to sort
 int numArray[] = {7, 12, 19, 3, 18, 4, 2, 6, 15, 8};
+
+// The number of values in the array
 int arraySize = sizeof(numArray) / sizeof(numArray[0]);
 
+// Algorithm to sort the array O(n^2)
 void* bubbleSort(void* argv){
 	int i;
 	int j;
+	int p;
 	
+	// Create a struct for part of the array
 	struct sort halfOfArray = *(struct sort*) argv;
 	int high = halfOfArray.upperBound;
 	int low = halfOfArray.lowerBound;
 	
-	for(i = low; i < high - 1; i++){
+	printf("This is array before:\n");
+	for(int p = low; p < high; p++){
+		printf("%d ", numArray[p]);
+	}
+	printf("\n\n");
+	
+	// Using Bubble Sort to sort array
+	for(i = 0; i < high - 1; i++){
 		
 		for(j = 0; j < high - i - 1; j++){
 			
@@ -30,18 +45,31 @@ void* bubbleSort(void* argv){
 			}
 		}
 	}
+	
+	
+	printf("This is array after:\n");
+	for(int p = low; p < high; p++){
+		printf("%d ", numArray[p]);
+	}
+	printf("\n\n");
+	
+	
 	return NULL;
 }
 
-
+// Merge both arrays together
 void* merge(void* argv){
+	
 	struct sort halfOfArray = *(struct sort*) argv;
 	int b[arraySize];
 	int i;
+	int j;
 	int currentIndex = 0;
 	int down = 0;
 	int up = halfOfArray.upperBound;
 	
+	// Merging arrays together if current value is greater
+	// than next value its comparing to
 	while (down < halfOfArray.upperBound && up < arraySize){
 		if(numArray[down] < numArray[up]){
 			b[currentIndex++] = numArray[down++];
@@ -60,9 +88,8 @@ void* merge(void* argv){
 		b[currentIndex] = numArray[up++];
 	}
 	
-	for(i = 0; i < arraySize; i++){
-		numArray[i] = b[i];
-	}
+	printf("\n");
+	
 	
 	return NULL;
 
@@ -71,16 +98,26 @@ void* merge(void* argv){
 }
 
 int main(){
-
+	// Creating two structs
 	struct sort half[2];
 	
+	// Settinng structs to certain parts
+	// of numArray
 	half[0].lowerBound = 0;
 	half[0].upperBound = arraySize / 2;
 	
 	half[1].lowerBound = half[0].upperBound;
 	half[1].upperBound = arraySize;
 	
+	// Creating 2 threads for both
+	// halves of arrays
 	pthread_t threads[2];
+	
+	printf("List Before sorting: \n");
+	for(int i = 0; i < arraySize; i++){
+		printf("%d ", numArray[i]);
+	}
+	printf("\n\n");
 	
 	for(int i = 0; i < 2; i++){
 		pthread_create(&threads[i], NULL, bubbleSort, &half[i]);
@@ -94,8 +131,9 @@ int main(){
 	pthread_create(&mergeThread, NULL, merge, &half[0]);
 	pthread_join(mergeThread, NULL);
 	
+	printf("Merging:\n");
 	for(int i = 0; i < arraySize; i++){
-		printf("%d, ", numArray[i]);
+		printf("%d ", numArray[i]);
 	}
 	
 	printf("\n\n");
